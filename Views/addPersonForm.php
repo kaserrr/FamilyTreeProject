744a2e621498b2b103a6c4c6f3a1f0c849024e20
kaserrr <99057099@mydavinci.nl>
2024-02-25 14:32:06 +0100

    commit #1

---
+++ Views/addPersonForm.php
@@ -0,0 +1,9 @@
+<div>
+    <h2>Add</h2>
+    <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname">
+    </form>
+</div>
